162c617073f837276d22fb98eb6f76975ad211c8
Alexander Rozhdestvenskiy <alexrozh1988@gmail.com>
2021-12-16 19:23:28 +0300

    Icon

---
+++ FirebaseApp/Controllers/TabBarViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarViewController.swift
+//  FirebaseApp
+//
+//  Created by Alexander Rozhdestvenskiy on 16.12.2021.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
