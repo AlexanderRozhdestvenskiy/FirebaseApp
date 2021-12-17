fe17f80782ebea642dbc2b9b0fc6738601c13b2b
Alexander Rozhdestvenskiy <alexrozh1988@gmail.com>
2021-12-17 13:16:47 +0300

    TwoFull

---
+++ FirebaseApp/Controllers/Auth/SignInViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignInViewController.swift
+//  FirebaseApp
+//
+//  Created by Alexander Rozhdestvenskiy on 17.12.2021.
+//
+
+import UIKit
+
+class SignInViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Sign In"
+        view.backgroundColor = .systemBackground
+    }
+    
+}
---
+++ FirebaseApp/Controllers/Auth/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  FirebaseApp
+//
+//  Created by Alexander Rozhdestvenskiy on 17.12.2021.
+//
+
+import UIKit
+
+class SignUpViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Create Account"
+        view.backgroundColor = .systemBackground
+    }
+    
+
+}
